b720ad14954c64f1a70d0589929aa1ec50a9b552
Andrey Konovod <konovod@gmail.com>
2021-12-29 16:48:45 +0300

    wip macro magic

--- spec/ctsm_spec.cr
+++ spec/ctsm_spec.cr
@@ -18,59 +18,58 @@ end
 
 class TestMachine2 < CTSM::Machine
   initial_state(First)
-  transition_if(flip1, First, to: Second)
-  transition_if(flip2, Second, to: First)
+  transition(flip1, First, to: Second)
+  transition(flip2, Second, to: First)
   transition(reset, Second, First, to: First)
 end
 
 describe CTSM do
   # TODO: Write tests
+  # it "state machine can be created" do
+  #   machine = TestMachine.new
+  #   machine.state.should eq TestMachine::State::Initial
+  # end
+  # it "transitions are possible" do
+  #   machine = TestMachine.new
+  #   machine.startup
+  #   machine.state.should eq TestMachine::State::First
+  #   expect_raises(CTSM::TransitionImpossible) { machine.startup }
+  # end
 
-  it "state machine can be created" do
-    machine = TestMachine.new
-    machine.state.should eq TestMachine::State::Initial
-  end
-  it "transitions are possible" do
-    machine = TestMachine.new
-    machine.startup
-    machine.state.should eq TestMachine::State::First
-    expect_raises(CTSM::TransitionImpossible) { machine.startup }
-  end
+  # it "transitions block is called" do
+  #   machine = TestMachine.new
+  #   machine.startup
+  #   machine.was_reset.should eq false
+  #   machine.reset
+  #   machine.was_reset.should eq true
+  #   machine.reset
+  #   machine.was_reset.should eq true
+  # end
 
-  it "transitions block is called" do
-    machine = TestMachine.new
-    machine.startup
-    machine.was_reset.should eq false
-    machine.reset
-    machine.was_reset.should eq true
-    machine.reset
-    machine.was_reset.should eq true
-  end
+  # it "different machines do not interact" do
+  #   m1 = TestMachine.new
+  #   m2 = TestMachine2.new
+  #   m1.state.should eq TestMachine::State::Initial
+  #   m2.state.should eq TestMachine::State::First
+  #   m1.startup
+  #   m2.flip1
+  #   m1.state.should eq TestMachine::State::First
+  #   m2.state.should eq TestMachine::State::Second
+  # end
 
-  it "different machines do not interact" do
-    m1 = TestMachine.new
-    m2 = TestMachine2.new
-    m1.state.should eq TestMachine::State::Initial
-    m2.state.should eq TestMachine::State::First
-    m1.startup
-    m2.flip1
-    m1.state.should eq TestMachine::State::First
-    m2.state.should eq TestMachine::State::Second
-  end
-
-  pending "transitions check is called" do
-    machine = TestMachine.new
-    machine.startup
-    machine.state.should eq TestMachine::State::First
-    machine.flip
-    machine.state.should eq TestMachine::State::First
-    machine.flip_possible = true
-    machine.flip
-    machine.state.should eq TestMachine::State::Second
-    machine.flip
-    machine.state.should eq TestMachine::State::First
-    machine.flip_possible = true
-    machine.flip
-    machine.state.should eq TestMachine::State::First
-  end
+  # pending "transitions check is called" do
+  #   machine = TestMachine.new
+  #   machine.startup
+  #   machine.state.should eq TestMachine::State::First
+  #   machine.flip
+  #   machine.state.should eq TestMachine::State::First
+  #   machine.flip_possible = true
+  #   machine.flip
+  #   machine.state.should eq TestMachine::State::Second
+  #   machine.flip
+  #   machine.state.should eq TestMachine::State::First
+  #   machine.flip_possible = true
+  #   machine.flip
+  #   machine.state.should eq TestMachine::State::First
+  # end
 end
--- src/ctsm.cr
+++ src/ctsm.cr
@@ -6,33 +6,90 @@ module CTSM
   class Machine
     macro initial_state(x)
       getter state = State::{{x}}
-    end
-
-    macro transition(method, *afrom, to, &)
-      def {{method}}
-        froms = {{afrom.map { |x| ("State::#{x}").id }}}
-        raise CTSM::TransitionImpossible.new("#{self.class}: Transition {{method}} impossible for state #{@state}") unless froms.includes? @state
-        @state = State::{{to}}
-        {{yield}}
+      {% if @type.methods.map(&.name).includes? "initial_state_defined" %}
+        {% raise "initial_state is already defined" %}
+      {% end %}
+      def initial_state_defined
       end
     end
 
-    macro transition_if(method, *afrom, to, &)
-      def {{method}}
-        froms = {{afrom.map { |x| ("State::#{x}").id }}}
-        raise CTSM::TransitionImpossible.new("#{self.class}: Transition {{method}} impossible for state #{@state}") unless froms.includes? @state
-        return unless {{yield}}
-        @state = State::{{to}}
-      end
+    macro transition(method, *afrom, to, &block)
+      {% if afrom.size > 0 %}
+        {% for from_state in afrom %}
+          private def transitioninternal_{{method}}_from_{{from_state}}_to_{{to}}
+            @state = State::{{to}}
+            {{yield}} 
+          end
+        {% end %}
+      {% else %} 
+        private def transitioninternal_{{method}}_fromall_to_{{to}}
+          @state = State::{{to}}
+          {{yield}} 
+        end
+      {% end %}
     end
 
+    macro transition_if(method, *afrom, to, &block)
+      {% if afrom.size > 0 %}
+        {% for from_state in afrom %}
+          private def transitioninternal_{{method}}_from_{{from_state}}_to_{{to}}
+            {% if block %}
+              return unless {{yield}}
+            {% else %}
+              {% raise "block must be present in `transition_if`" %}
+            {% end %}
+            @state = State::{{to}}
+          end
+        {% end %}
+      {% else %} 
+        private def transitioninternal_{{method}}_fromall_to_{{to}}
+          {% if block %}
+            return unless {{yield}}
+          {% else %}
+            {% raise "block must be present in `transition_if`" %}
+          {% end %}
+          @state = State::{{to}}
+        end
+      {% end %}
+    end
+
+    macro inherited
+    {% verbatim do %}
     macro finished
+      {% if !@type.methods.map(&.name.stringify).includes?("initial_state_defined") %}
+        {% raise "initial_state is not defined" %}
+      {% end %}
+      # gather list of states
+      {% states = {} of String => Bool %}
+      {% for meth in @type.methods %}
+        {% name_parts = meth.name.split('_') %}
+        {% if name_parts[0] == "transitioninternal" %}
+          # transitioninternal_method_from_fromstate_to_tostate
+          # transitioninternal_method_fromall_to_tostate
+          {% if name_parts[2] == "fromall" %}
+            {% states[name_parts[4]] = true %}
+          {% else %}  
+            {% states[name_parts[3]] = true %}
+            {% states[name_parts[5]] = true %}
+          {% end %}
+        {% end %}
+      {% end %}
+      {% states = states.keys.map(&.capitalize) %}
+      # now define enum  
+      {% begin %}
       enum State
-        Initial
-        First
-        Second
+        {% for state in states %}
+          {{state.id}}
+        {% end %}
       end
-      
+      {% end %}
+      {% matrix = [] of Array(String) %}
+      {% any_transitions = [] of String %}
+
+
+
+    end     
+    {% end %}
     end
   end
 end
