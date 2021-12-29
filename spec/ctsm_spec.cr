ceda1287e1f91740056c95075fa3ce91ce89dc13
Andrey Konovod <konovod@gmail.com>
2021-12-29 14:00:53 +0300

    first spec

--- spec/ctsm_spec.cr
+++ spec/ctsm_spec.cr
@@ -1,9 +1,31 @@
 require "./spec_helper"
 
-describe Ctsm do
+class TestMachine < CTSM::Machine
+  property was_reset = false
+  initial_state(Initial)
+  transition(startup, Initial, First)
+  # transition_if(flip, First, to: Second) do
+  #   @ticks_passed > 1000
+  # end
+  # transition_if(flip, Second, to: First) do
+  #   @ticks_passed > 1000
+  # end
+  # transition(reset, Second, First, to: First) do
+  #   @was_reset = true
+  # end
+end
+
+describe CTSM do
   # TODO: Write tests
 
-  it "works" do
-    false.should eq(true)
+  it "state machine can be created" do
+    machine = TestMachine.new
+    machine.state.should eq TestMachine::State::Initial
+  end
+  it "transitions are possible" do
+    machine = TestMachine.new
+    machine.startup
+    machine.state.should eq TestMachine::State::First
+    expect_raises(CTSM::TransitionImpossible) { machine.startup }
   end
 end
--- src/ctsm.cr
+++ src/ctsm.cr
@@ -1,6 +1,29 @@
 # TODO: Write documentation for `Ctsm`
-module Ctsm
-  VERSION = "0.1.0"
+module CTSM
+  class TransitionImpossible < Exception
+  end
 
-  # TODO: Put your code here
+  class Machine
+    macro initial_state(x)
+    end
+
+    macro transition(method, afrom, ato, &)
+      def {{method}}
+        raise CTSM::TransitionImpossible.new("#{self.class}: Transition {{method}} impossible for state #{@state}") unless @state == State::{{afrom}}
+        @state = State::{{ato}}
+      end
+    end
+
+    macro transition_if(method, afrom, ato, &)
+    end
+
+    macro finished
+      enum State
+        Initial
+        First
+        Second
+      end
+      getter state = State::Initial
+    end
+  end
 end
